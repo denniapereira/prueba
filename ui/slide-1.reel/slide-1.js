8abae29249666a47752e558c670413ceec1683eb
Dennia Pereira <denniapereira@gmail.com>
2015-11-18 22:28:49 +0000

    Add component slide 1

---
+++ ui/slide-1.reel/slide-1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/slide-1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Slide-1
+ * @extends Component
+ */
+exports.Slide-1 = Component.specialize(/** @lends Slide-1# */ {
+    constructor: {
+        value: function Slide-1() {
+            this.super();
+        }
+    }
+});
