75f2cecfa6f958c7c97d7a71110944298784bb4d
Dennia Pereira <denniapereira@gmail.com>
2015-11-18 22:28:28 +0000

    Add module Módulo 1

---
+++ modulo-1.js
@@ -0,0 +1,15 @@
+/**
+ * @module //modulo-1
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Modulo-1
+ * @extends Montage
+ */
+exports.Modulo-1 = Montage.specialize(/** @lends Modulo-1# */ {
+    constructor: {
+        value: function Modulo-1() {
+            this.super();
+        }
+    }
+});
